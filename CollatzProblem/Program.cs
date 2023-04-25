70b4e59d00f79f9e325c29cf973393f53c4bc38d
ahint <ahint@FASTCAT>
2023-04-25 14:57:00 +0200

    Add project files.

---
+++ CollatzProblem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CollatzProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long maxNumber = 1000000; // Maximum number to consider
+            long longestChainNumber = 0; // Number with the longest chain
+            int longestChainLength = 0; // Length of the longest chain
+
+            for (long i = 2; i < maxNumber; i++)
+            {
+                long n = i;
+                int chainLength = 1;
+
+                // Generate the Collatz sequence
+                while (n != 1)
+                {
+                    if (n % 2 == 0)
+                    {
+                        n = n / 2;
+                    }
+                    else
+                    {
+                        n = 3 * n + 1;
+                    }
+                    chainLength++;
+                }
+
+                // Update longest chain if necessary
+                if (chainLength > longestChainLength)
+                {
+                    longestChainLength = chainLength;
+                    longestChainNumber = i;
+                }
+            }
+
+            Console.WriteLine($"The starting number under {maxNumber} that produces the longest chain is: {longestChainNumber}");
+            Console.WriteLine($"The length of the longest chain is: {longestChainLength}");
+        }
+    }
+}
